29bb85622f8590feddd477f2dd928a279fde95c4
Paulo Cuellas <pamcuellas@gmail.com>
2019-03-05 10:41:01 -0500

    Traing to solve CORS problem.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';  
-import animaWords from '../services/animaWords';
-import { loadReCaptcha } from 'react-recaptcha-google';
-import { ReCaptcha } from 'react-recaptcha-google';
+import animaWords 			from '../services/animaWords';
+import { loadReCaptcha } 	from 'react-recaptcha-google';
+import { ReCaptcha } 		from 'react-recaptcha-google';
+import axios 				from 'axios';
 
 
 
@@ -9,34 +10,129 @@ class Contact extends Component {
 
 	constructor(props, context) {
 		super(props, context);
-	    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
+		this.state = {
+			captchaExpired: true,
+			captchaToken:''
+		}
 	    this.verifyCallback = this.verifyCallback.bind(this);
+	    this.handlingErrors = this.handlingErrors.bind(this);
+	    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
+	    this.recaptchaExpired = this.recaptchaExpired.bind(this);
 	}
 
 
 	componentDidMount(){
 		 loadReCaptcha();
-		if (this.myCaptcha) {
+		if (this.Captcha) {
 	        console.log("started, just a second...")
-	        this.myCaptcha.reset();
-	        //this.myCaptcha.execute();
+	        this.Captcha.reset();
+	        //this.Captcha.execute();
 	    }
 		var words = document.querySelectorAll('.word');
 		animaWords(words);
 	}
 
-  onLoadRecaptcha() {
-  	console.log("onLoadRecaptcha");
-      if (this.myCaptcha) {
-          this.myCaptcha.reset();
-          //this.myCaptcha.execute();
-      }
-  }
+	onLoadRecaptcha() {
+		console.log("onLoadRecaptcha");
+	  if (this.Captcha) {
+	      this.Captcha.reset();
+	      //this.Captcha.execute();
+	  }
+	}
 
-  verifyCallback(recaptchaToken) {
-    // Here you will get the final recaptchaToken!!!  
-    console.log("<= your recaptcha token", recaptchaToken )
-  }
+	verifyCallback(recaptchaToken) {
+		if (recaptchaToken.length > 0) {
+			// Lets verify token
+			const googleURL = process.env.REACT_APP_GOOGLE_API_URL;
+			// const KEY = process.env.REACT_APP_TST_SECRET_SITE_KEY;
+			const KEY = process.env.REACT_APP_HRK_SECRET_SITE_KEY;
+			console.log("PARAMS " + googleURL + " " + KEY );
+
+			axios({
+				method: 'POST',
+	            url: `${googleURL}`,
+	            withCredentials: true,
+	            params: {
+	                secret: `${KEY}`,
+	                response: recaptchaToken
+	            },
+	            headers: {
+	            	"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
+	            }
+			})
+			.then( data => {
+					console.log("======================================================================== ", data);
+			})
+			.catch( this.handlingErrors );
+
+		} 
+
+
+
+
+/*
+			axios
+			.get(`https://api.unsplash.com/search/photos/?page=${page}&per_page=30&query=${query}&client_id=${KEY}`)	
+			.then(data => {
+				this.setState({ pictures: data.data.results, query: query });
+			})
+			.catch(err => {
+				console.log('Unfortunately we got something wrong! ', err);
+			});		
+*/
+/*
+			axios.post(
+				`${googleURL}`,
+				{
+					data: {
+						secret: `${KEY}`,   // Where postId is the  field name, much better than use https://jsonplaceholder.typicode.com/comments?postId=2 on the URL.
+						response: recaptchaToken
+					},
+					
+					headers: {	
+								contentType: 'application/json', //application/x-www-form-urlencoded',
+								'Access-Control-Allow-Origin': '*',
+								'Access-Control-Allow-Methods': 'POST'
+							}
+				} 
+			)
+			.then( data => {
+					console.log("======================================================================== ", data);
+			})
+			.catch( this.handlingErrors );
+		}
+*/
+
+/*
+ $.ajax({
+        type: "POST",
+        url: 'https://www.google.com/recaptcha/api/siteverify',
+        data: {"secret" : "(your-secret-key)", "response" : response, "remoteip":"localhost"},
+        contentType: 'application/x-www-form-urlencoded',
+        success: function(data) { console.log(data); }
+    });
+*/
+	// Here you will get the final recaptchaToken!!!  
+	console.log("<= your recaptcha token", recaptchaToken );
+
+	}
+
+		/* Axios can distinguish errors */ 
+		handlingErrors = (error) => {
+			if ( error.response ) {
+				console.log("Something was wrong with RESPONSE ==>", error.response.status);
+			} else if ( error.request ) {
+				console.log("Something was wrong with REQUEST ==>", error.request);
+			} else { 
+				console.log("Something was wrong ==>", error.message);
+			}
+		} 
+
+
+
+	recaptchaExpired(){
+		console.log("ReCaptcha Expired!");
+	}
 
 	componentWillUnmount() {
 		clearTimeout();
@@ -44,7 +140,7 @@ class Contact extends Component {
 
 	render() {  
 
-		//const KEY = process.env.REACT_APP_TST_SITE_KEY;	
+		// const KEY = process.env.REACT_APP_TST_SITE_KEY;	
 		const KEY = process.env.REACT_APP_HRK_SITE_KEY;	
 
 		return (
@@ -83,31 +179,25 @@ class Contact extends Component {
 					</p>
 				</div>
 				<hr></hr>
-				<div className="content-content-area">
-					<h3 className="title">ReCaptcha Here</h3>	
-      <div>
-        <ReCaptcha
-            ref={(el) => {this.myCaptcha = el;}}
-            size="compact"
-            render="explicit"
-            theme="dark"
-            sitekey={`${KEY}`}
-            onloadCallback={this.onLoadRecaptcha}
-            verifyCallback={this.verifyCallback}
-        />
-        <code>
-          1. Add <strong>your site key</strong> in the ReCaptcha component. <br/>
-          2. Check <strong>console</strong> to see the token.
-        </code>
-      </div>
-
-
-
-<hr />
-
+				<h3 className="title">ReCaptcha Here</h3>	
+				<div>
+					<ReCaptcha
+					ref={(el) => {this.Captcha = el;}}
+					size="compact"
+					render="explicit"
+					theme="dark"
+					sitekey={`${KEY}`}
+					onloadCallback={this.onLoadRecaptcha}
+					verifyCallback={this.verifyCallback}
+					expiredCallback={this.recaptchaExpired}
+					/>
+					<code>
+					1. Add <strong>your site key</strong> in the ReCaptcha component. <br/>
+					2. Check <strong>console</strong> to see the token.
+					</code>
 				</div>
+				<hr />
 			</div>
-
 		);
 	}	
 
