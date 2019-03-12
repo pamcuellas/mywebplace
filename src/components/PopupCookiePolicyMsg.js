a904fb774d27717d4ac541b60da56a6133203d5f
Paulo Cuellas <pamcuellas@gmail.com>
2019-03-12 18:11:48 -0400

    Invisible reCaptcha included in Gallery.

--- src/components/PopupCookiePolicyMsg.js
+++ src/components/PopupCookiePolicyMsg.js
@@ -9,9 +9,9 @@ class PopupCookie extends Component {
 		this.handleClick = this.handleClick.bind(this); 
 	}
 
-	handleClick() {
-	 	createReq( 'PolicyVisualized' ); 	
-	}
+	// handleClick() {
+	//  	createReq( 'PolicyVisualized' ); 	
+	// }
 
 	render() {
 
@@ -21,7 +21,7 @@ class PopupCookie extends Component {
 	            <div className="modal-title">
 		            <h1>We use cookies and similar technologies to run this website and help us understand how you use it.</h1>
 		        </div>    
-	        	<a onClick={this.handleClick} className="popup-cookie" href="https://termsfeed.com/cookies-policy/3b5607a3c360a5a1b8ac4b372c240605" target="_blank" rel="noopener noreferrer">Click here to see our cookie policy.</a>
+	        	<a className="popup-cookie" href="https://termsfeed.com/cookies-policy/3b5607a3c360a5a1b8ac4b372c240605" target="_blank" rel="noopener noreferrer">Click here to see our cookie policy.</a>
 	        	<div className="close-button">
 		        	<i className="fas fa-check-circle"></i> 
 		        	<span>Got it!</span>
